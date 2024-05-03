9abb26d062ebd7eea10ccfe20d240008b09cedb9
Frozen Forest Reality Technologies <frozenforeststudio@gmail.com>
2024-05-03 15:34:55 +0300

    Update FF_HTTP_LWS_Enums.h

--- Source/FF_HTTP_LWS/Public/FF_HTTP_LWS_Enums.h
+++ Source/FF_HTTP_LWS/Public/FF_HTTP_LWS_Enums.h
@@ -3,7 +3,7 @@
 #include "CoreMinimal.h"
 
 UENUM(BlueprintType)
-enum class ELwsReasons : uint8
+enum class ELwsCallbacks : uint8
 {
 	Lws_Callback_None							UMETA(DisplayName = "Lws_Callback_None"),
 	Lws_Callback_Http							UMETA(DisplayName = "LWS Callback HTTP"),
@@ -11,4 +11,20 @@ enum class ELwsReasons : uint8
 	Lws_Callback_Http_Body_Completion			UMETA(DisplayName = "Lws_Callback_Http_Body_Completion"),
 	Lws_Callback_Http_Writable					UMETA(DisplayName = "Lws_Callback_Http_Writable"),
 };
-ENUM_CLASS_FLAGS(ELwsReasons)
+ENUM_CLASS_FLAGS(ELwsCallbacks)
+
+UENUM(BlueprintType)
+enum class ELwsKnownHeaders : uint8
+{
+	Lws_Header_Origin							UMETA(DisplayName = "Lws Header Origin"),
+	Lws_Header_Content_Lenght					UMETA(DisplayName = "Lws Header Content Lenght"),
+	Lws_Header_Content_Type						UMETA(DisplayName = "Lws Header Content Type"),
+	Lws_Header_Host								UMETA(DisplayName = "Lws Header Host"),
+	Lws_Header_User_Agent						UMETA(DisplayName = "Lws Header User Agent"),
+	Lws_Header_Accept							UMETA(DisplayName = "Lws Header Accept"),
+	Lws_Header_Accept_Encoding					UMETA(DisplayName = "Lws Header Accept Encoding"),
+	Lws_Header_Connection						UMETA(DisplayName = "Lws Header Connection"),
+	Lws_Header_Authentication					UMETA(DisplayName = "Lws Header	Authentication"),
+
+};
+ENUM_CLASS_FLAGS(ELwsKnownHeaders)
